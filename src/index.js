47dbae1d8bc064f907026c9f845d70e707839e34
Тимур Ахметзянов <dijer@mail.ru>
2020-12-22 23:46:30 +0300

    add data.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3001;
 const app = express();
@@ -8,14 +11,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+
 let data = {};
-app.post('/counter/:bookId/incr', (req, res) => {
+
+const filepath = path.join(__dirname, 'data.json');
+fs.readFile(filepath, 'utf-8', (err, data) => {
+    if (err) throw new Error(err);
+    const counter = JSON.parse(data);
+    data = counter;
+});
+
+app.post('/counter/:bookId/incr', async (req, res) => {
     const { bookId } = req.params;
     if (!data[bookId]) {
         data[bookId] = 0;
     }
     data[bookId] += 1;
-    res.status(200).json('ok');
+    await fs.writeFile(filepath, JSON.stringify(data), (err) => {
+        if (err) throw new Error(err);
+    });
+    res.status(200).json(data[bookId]);
 });
 
 app.get('/counter/:bookId', (req, res) => {
